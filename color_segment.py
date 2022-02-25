3c2fbc45aae44a2540d761eef6760c7011cc080b
shiyaqiu <shiyaqiu@vizvision.com>
2022-02-25 18:02:27 +0800

    实现两种分割方法

--- color_segment.py
+++ color_segment.py
@@ -5,31 +5,45 @@ import numpy as np
 from utils import *
 
 
+def color_segment_1(image, min_size=20):
+    image_clean = clean_background(np.array(image), min_size=min_size)
+
+    image_clean = cv2.GaussianBlur(image_clean, (5, 5), 1)
+    gray = cv2.cvtColor(image_clean, cv2.COLOR_BGR2GRAY)
+    rects = get_rects_from_gray_image(gray, min_size=min_size)
+    return rects
+
+
+def color_segment_2(image, min_size=20, min_area=400):
+    image_clean = clean_background(np.array(image), min_size=min_size)
+
+    colors, masks = get_sub_masks(image_clean, min_area=min_area)
+    masks = merge_masks(colors, masks)
+    masks = filter_edge_mask(masks)
+
+    rects = []
+    for mask in masks:
+        rects += get_rects_from_mask(mask, min_size=20)
+    return rects
 
 
 
 if __name__ == '__main__':
     source_dir = 'images'
-    result_dir = 'results'
-    clean_images_dir = 'clean_images'
+    result_dir = 'results-1'
 
     image_names = os.listdir(source_dir)
-
-    for image_name in image_names[1:2]:
+    start_all = time.time()
+    for image_name in image_names[:20]:
         if not image_name.endswith(('.png', '.jpg', '.jpeg')):continue
         image_path = os.path.join(source_dir, image_name)
         image = cv2.imread(image_path)
-        image_clean = clean_background(image, min_size=20)
+        
+        rects = color_segment_1(image)
+        # rects = color_segment_2(image)
 
-        colors, masks = get_sub_masks(image_clean, min_area=400)
+        for (x1, y1, x2, y2) in rects:
+            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        cv2.imwrite(os.path.join(result_dir, image_name), image)
 
-        for color, mask in zip(colors, masks):
-            print(color)
-            cv2.imshow('mask', mask)
-            cv2.waitKey(0)
-
-
-        # for (x1, y1, x2, y2) in rects:
-        #     cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
-        # cv2.imwrite(os.path.join(result_dir, image_name), image)
-        # cv2.imwrite(os.path.join(clean_images_dir, image_name), image_clean)
+    print(f'mean time: {(time.time() - start_all) / 20}')
--- utils.py
+++ utils.py
@@ -29,7 +29,7 @@ def get_sub_masks(image, min_area=500):
     for i in range(h):
         for j in range(w):
             color = image[i, j, :]
-            if in_color_range(color, [[0, 5], [0, 5], [0, 5]]):continue
+            if is_in_color_range(color, [[0, 5], [0, 5], [0, 5]]):continue
             color_str = f'{color[0]},{color[1]},{color[2]}'
             if color_str not in color_to_idx:
                 mask = cv2.inRange(image, color, color)
@@ -37,7 +37,7 @@ def get_sub_masks(image, min_area=500):
                     color_to_idx[color_str] = -1
                 else:
                     masks.append(mask)
-                    colors.append(color_str)
+                    colors.append(list(color))
                     color_to_idx[color_str] = idx
                     id_to_color[idx] = color_str
                     idx += 1
@@ -45,42 +45,86 @@ def get_sub_masks(image, min_area=500):
     return colors, masks
 
 
-def merge_masks(colors, masks):
-    mask_dict = {}
-    for idx, color in enumerate(colors):
-        mask_dict[color] = masks[idx]
-    
-    diff_colors = [[127, 127, 127],
-                   [128, 128, 128],
-                   [128, 127, 127],
-                   [127, 128, 127],
-                   [127, 127, 128],
-                   [127, 128, 128],
-                   [128, 127, 128],
-                   [128, 128, 127],
-                   [-127, -127, -127],
-                   [-128, -128, -128],
-                   [-128, -127, -127],
-                   [-127, -128, -127],
-                   [-127, -127, -128],
-                   [-127, -128, -128],
-                   [-128, -127, -128],
-                   [-128, -128, -127]]
+def merge_colors(colors, deviation=5, offset=None):
+    merged_colors = []
+    single_colors = []
 
-    meraged_masks = []
-    for i, color in enumerate(colors):
-        color_value = [int(v) for v in color.split(',')]
+    while len(colors):
+        color = colors.pop()
+        sub_colors = [color]
         
-        for db, dg, dr in diff_colors:
-            vb = max(min(color_value[0] + db, 255), 0)
-            vg = max(min(color_value[1] + dg, 255), 0)
-            vr = max(min(color_value[2] + dr, 255), 0)
-            search_color = f'{vb},{vg},{vr}'
-            if search_color in colors:
-                meraged_masks.append(mask_dict[color]+mask_dict[search_color])
-                colors.pop(i)
-                break
-    return meraged_masks
+        base_colors = []
+        if offset is not None: 
+            for off in offset:
+                b_ = color[0] + off
+                g_ = color[1] + off
+                r_ = color[2] + off
+                if max(b_, g_, r_) <= 256 and min(b_, g_, r_) > -2:
+                    base_colors.append([b_, g_, r_])
+        else:
+            base_colors.append(color)
+
+        for (b, g, r) in base_colors:
+            b_range = [max(0, b-deviation), min(255, b+deviation)]
+            g_range = [max(0, g-deviation), min(255, g+deviation)]
+            r_range = [max(0, r-deviation), min(255, r+deviation)]
+    
+            color_range = [b_range, g_range, r_range]
+            
+            for idx, color_ in enumerate(colors):
+                if is_in_color_range(color_, color_range):
+                    colors.pop(idx)
+                    sub_colors.append(color_)
+
+        if len(sub_colors) > 1:
+            merged_colors.append(sub_colors)
+        else:
+            single_colors.append(sub_colors[0])
+    return merged_colors, single_colors
+
+
+def merge_masks(colors, masks):
+
+    color_dict = {}
+    color_idx = 0
+    for (b, g, r) in colors:
+        color_str = f'{b},{g},{r}'
+        color_dict[color_str] = color_idx
+        color_idx += 1
+    
+    merged_colors, single_colors = merge_colors(colors, deviation=5, offset=[127, -127])
+    
+    output_masks = []
+    for (b, g, r) in single_colors:
+        color_str = f'{b},{g},{r}'
+        output_masks.append(masks[color_dict[color_str]])
+    
+    for mask_group in merged_colors:
+        (b, g, r) = mask_group[0]
+        color_str = f'{b},{g},{r}'
+        base_mask = masks[color_dict[color_str]]
+        for (b, g, r) in mask_group[1:]:
+            color_str = f'{b},{g},{r}'
+            base_mask += masks[color_dict[color_str]]
+        output_masks.append(base_mask)
+    
+    return output_masks
+
+
+def filter_edge_mask(masks):
+    filtered_masks = []
+    for mask in masks:
+        mask_ = cv2.GaussianBlur(np.array(mask), (5, 5), 1)
+        ret, thresh = cv2.threshold(mask_, 0, 200, cv2.THRESH_BINARY)
+        kernel = np.ones((15, 15),np.uint8)
+        thresh = cv2.erode(thresh ,kernel)
+        if np.sum(thresh>0) > 50:
+            filtered_masks.append(mask)
+    return filtered_masks
+
+
+def nms(rects):
+    return rects
 
 
 def iou(rect1, rect2):
@@ -139,6 +183,28 @@ def clean_background(image, min_size=10):
     return image
 
 
+def get_rects_from_mask(mask, min_size=20):
+    
+    image_h, image_w = mask.shape[:2]
+    mask = cv2.GaussianBlur(mask, (5, 5), 1)
+
+    ret, thresh = cv2.threshold(mask, 0, 200, cv2.THRESH_BINARY)
+    kernel = np.ones((7, 7),np.uint8)
+    thresh = cv2.dilate(thresh ,kernel)
+    
+    rects = []
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        if w < min_size or h < min_size:continue
+        x1 = max(0, x)
+        y1 = max(0, y)
+        x2 = min(image_w-1, x+w)
+        y2 = min(image_h-1, y+h)
+        rects.append([x1, y1, x2, y2])
+    return rects
+
 
 def get_rects_from_gray_image(gray_image, min_size=20):
     
@@ -162,13 +228,13 @@ def get_rects_from_gray_image(gray_image, min_size=20):
 
 
 
-def in_color_range(color, color_range):
+def is_in_color_range(color, color_range):
     assert len(color) == len(color_range)
     
     for i in range(len(color)):
         v_range = color_range[i]
         assert len(v_range) == 2
         assert v_range[0] < v_range[1]
-        if color[i] not in range(v_range[0], v_range[1]):return False
+        if color[i] not in range(v_range[0], v_range[1]+1):return False
         
     return True
