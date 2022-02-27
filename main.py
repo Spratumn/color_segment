09854c2976398dba475d7997e4f7b153e8fcaac2
shiyaqiu <spratumn@163.com>
2022-02-27 21:12:17 +0800

    添加设置图像尺寸ui

--- main.py
+++ main.py
@@ -4,52 +4,33 @@
 import cv2
 import numpy as np
 import sys
-from PySide2.QtWidgets import QMainWindow, QMessageBox, QFileDialog, QApplication
+from PySide2.QtWidgets import QMainWindow, QFileDialog, QApplication
 from PySide2.QtGui import QImage, QPixmap
 from PySide2.QtCore import Qt
 
 from ui.ui_color_segment import Ui_MainWindow
-from color_segment import color_segment_1, color_segment_2, save_result_to_txt
 
 
-# 主窗口类，继承窗口Ui_MainWindow主界面
 class MyPS(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
-        # 窗口属性修改
         self.label.setAlignment(Qt.AlignCenter)
 
-        # 实例属性初始化
         self.fName = ''
+        self.center = [450, 335]
+        self.left_top = [450, 335]
+        self.bottom_right = [451, 336]
         self.mat = np.zeros([1, 1, 3], np.uint8)
-        # 信号槽连接
-        self.actionOpen.triggered.connect(self.load_file)
-        self.actionMethod1.triggered.connect(self.color_segment1)
-        self.actionMethod2.triggered.connect(self.color_segment2)
-        self.method = 1
-
-    def color_segment1(self):
-        rects = color_segment_1(self.mat)
-        image = self.draw_rects(rects, np.array(self.mat))
-        self.display_image(image)
-
-    def color_segment2(self):
-        rects = color_segment_2(self.mat)
-        image = self.draw_rects(rects, np.array(self.mat))
-        self.display_image(image)
+        self.resize_rate = 1.0
+        self.points = []
+        self.point_nums = 2
         
-    def draw_rects(self, rects, image):
-        for (x1, y1, x2, y2) in rects:
-            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
-        return image
-
-
-    # 加载文件,根据后缀判断文件类型
+        self.actionOpen.triggered.connect(self.load_file)
+        
     def load_file(self):
-        self.fName = QFileDialog.getOpenFileName(self, 'Open File', './',
-                                                 'Image Files (*.png *.jpg *.bmp)')
+        self.fName = QFileDialog.getOpenFileName(self, 'Open File', './', 'Image Files (*.png *.jpg *.bmp)')
         if self.fName[0].endswith(('.png', '.jpg', '.bmp')):
             self.mat = cv2.imread(self.fName[0])
             self.display_image(self.mat)
@@ -58,21 +39,32 @@ class MyPS(QMainWindow, Ui_MainWindow):
     def display_image(self, image):
         height, width, channels = image.shape
         if height > 400 or width > 800:
-            rate = height / 400 if height / 400 > width / 800 else width / 800
-            width = int(width / rate)
-            height = int(height / rate)
-            img = cv2.resize(image, (width, height))
+            self.resize_rate = height / 400 if height / 400 > width / 800 else width / 800
+            w = int(width / self.resize_rate)
+            h = int(height / self.resize_rate)
+            img = cv2.resize(image, (w, h))
         else:
             img = image
-        self.statusbar.showMessage('img size:({},{})'.format(width, height))
-        self.statusbar.show()
-        line_bytes = channels * width
+        line_bytes = channels * w
         qt_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        label_image: QImage = QImage(qt_image.data, width, height, line_bytes, QImage.Format_RGB888)
+        label_image: QImage = QImage(qt_image.data, w, h, line_bytes, QImage.Format_RGB888)
         self.label.setPixmap(QPixmap.fromImage(label_image))
+        self.left_top = [self.center[0]-w//2, self.center[1]-h//2]
+        self.bottom_right = [self.left_top[0]+w, self.left_top[1]+h]
 
     def mousePressEvent(self, event):
-        print('clicked at (', event.x(), ',', event.y(), ')')
+        (x1, y1), (x2, y2) = self.left_top, self.bottom_right
+        x, y = event.x(), event.y()
+
+        if x1<= x < x2 and y1 <= y < y2:
+            x = int((x - x1) * self.resize_rate)
+            y = int((y - y1) * self.resize_rate)
+            self.points.append([x, y])
+            if len(self.points) > self.point_nums: self.points.pop(0)
+            image_draw = np.array(self.mat)
+            for (px, py) in self.points:
+                cv2.circle(image_draw, (px, py), 2, (0, 0, 255), 2)
+            self.display_image(image_draw)
 
     def closeEvent(self, *args, **kwargs):
         pass
--- ui/ui_color_segment.py
+++ ui/ui_color_segment.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'color_segmentFcWqwg.ui'
+## Form generated from reading UI file 'color_segmentjcnNBu.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
@@ -25,18 +25,68 @@ class Ui_MainWindow(object):
         self.actionOpen = QAction(MainWindow)
         self.actionOpen.setObjectName(u"actionOpen")
         self.actionOpen.setShortcutContext(Qt.WindowShortcut)
-        self.actionMethod1 = QAction(MainWindow)
-        self.actionMethod1.setObjectName(u"actionMethod1")
-        self.actionMethod2 = QAction(MainWindow)
-        self.actionMethod2.setObjectName(u"actionMethod2")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.gridLayout = QGridLayout(self.centralwidget)
+        self.widget = QWidget(self.centralwidget)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(8, 9, 884, 571))
+        self.gridLayout = QGridLayout(self.widget)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.label = QLabel(self.centralwidget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.label = QLabel(self.widget)
         self.label.setObjectName(u"label")
+        self.label.setMinimumSize(QSize(882, 520))
+        self.label.setMaximumSize(QSize(882, 520))
 
-        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.label, 2, 0, 1, 1)
+
+        self.gridLayout_2 = QGridLayout()
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.label_3 = QLabel(self.widget)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setMaximumSize(QSize(42, 23))
+
+        self.gridLayout_2.addWidget(self.label_3, 0, 3, 1, 1)
+
+        self.pushButtonNext = QPushButton(self.widget)
+        self.pushButtonNext.setObjectName(u"pushButtonNext")
+        self.pushButtonNext.setMaximumSize(QSize(40, 23))
+
+        self.gridLayout_2.addWidget(self.pushButtonNext, 0, 6, 1, 1)
+
+        self.label_2 = QLabel(self.widget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setMinimumSize(QSize(550, 0))
+        self.label_2.setMaximumSize(QSize(40, 23))
+
+        self.gridLayout_2.addWidget(self.label_2, 0, 0, 1, 1)
+
+        self.spinBoxWidth = QSpinBox(self.widget)
+        self.spinBoxWidth.setObjectName(u"spinBoxWidth")
+        self.spinBoxWidth.setMaximumSize(QSize(50, 23))
+
+        self.gridLayout_2.addWidget(self.spinBoxWidth, 0, 2, 1, 1)
+
+        self.pushButtonLast = QPushButton(self.widget)
+        self.pushButtonLast.setObjectName(u"pushButtonLast")
+        self.pushButtonLast.setMaximumSize(QSize(40, 23))
+
+        self.gridLayout_2.addWidget(self.pushButtonLast, 0, 5, 1, 1)
+
+        self.spinBoxHeight = QSpinBox(self.widget)
+        self.spinBoxHeight.setObjectName(u"spinBoxHeight")
+        self.spinBoxHeight.setMaximumSize(QSize(50, 23))
+
+        self.gridLayout_2.addWidget(self.spinBoxHeight, 0, 4, 1, 1)
+
+        self.label_4 = QLabel(self.widget)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setMaximumSize(QSize(40, 23))
+
+        self.gridLayout_2.addWidget(self.label_4, 0, 1, 1, 1)
+
+
+        self.gridLayout.addLayout(self.gridLayout_2, 1, 0, 1, 1)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
@@ -44,19 +94,14 @@ class Ui_MainWindow(object):
         self.menubar.setGeometry(QRect(0, 0, 900, 23))
         self.menu = QMenu(self.menubar)
         self.menu.setObjectName(u"menu")
-        self.menuSegment = QMenu(self.menubar)
-        self.menuSegment.setObjectName(u"menuSegment")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
         self.menubar.addAction(self.menu.menuAction())
-        self.menubar.addAction(self.menuSegment.menuAction())
         self.menu.addAction(self.actionOpen)
         self.menu.addAction(self.actionExit)
-        self.menuSegment.addAction(self.actionMethod1)
-        self.menuSegment.addAction(self.actionMethod2)
 
         self.retranslateUi(MainWindow)
         self.actionExit.triggered.connect(MainWindow.close)
@@ -74,10 +119,12 @@ class Ui_MainWindow(object):
 #if QT_CONFIG(shortcut)
         self.actionOpen.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+O", None))
 #endif // QT_CONFIG(shortcut)
-        self.actionMethod1.setText(QCoreApplication.translate("MainWindow", u"Method1", None))
-        self.actionMethod2.setText(QCoreApplication.translate("MainWindow", u"Method2", None))
         self.label.setText("")
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Height:", None))
+        self.pushButtonNext.setText(QCoreApplication.translate("MainWindow", u">", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Set the image size:", None))
+        self.pushButtonLast.setText(QCoreApplication.translate("MainWindow", u"<", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Width:", None))
         self.menu.setTitle(QCoreApplication.translate("MainWindow", u"File", None))
-        self.menuSegment.setTitle(QCoreApplication.translate("MainWindow", u"Segment", None))
     # retranslateUi
 
